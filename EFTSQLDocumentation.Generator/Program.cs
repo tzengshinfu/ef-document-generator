203a3a673664d290da8a53bee180c6284669fb57
tzengshinfu <tzengshinfu@gmail.com>
2020-03-27 13:13:19 +0800

    Add table/column summary to t4 template files.

--- EFTSQLDocumentation.Generator/Program.cs
+++ EFTSQLDocumentation.Generator/Program.cs
@@ -1,176 +1,201 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CommandLineParser.Arguments;
+using CommandLineParser.Exceptions;
+using System;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
-using CommandLineParser.Arguments;
-using CommandLineParser.Exceptions;
 
-namespace EFTSQLDocumentation.Generator
-{
+namespace EFTSQLDocumentation.Generator {
+    class Program : IDisposable {
+        static void Main(string[] args) {
+            var parser = CreateParser();
 
-    class Program : IDisposable
-    {
-        static void Main(string[] args)
-        {
-            CommandLineParser.CommandLineParser parser = CreateParser();
-
-            try
-            {
+            try {
                 parser.ParseCommandLine(args);
             }
-            catch (CommandLineException e)
-            {
+            catch (CommandLineException e) {
                 Console.WriteLine(e.Message);
                 parser.ShowUsage();
+
                 return;
             }
 
-
-            String connectionString = ((ValueArgument<SqlConnectionStringBuilder>)parser.LookupArgument("connectionString")).Value.ConnectionString;
-            String inputFileName = ((FileArgument)parser.LookupArgument("input")).Value.FullName;
-            String outputFileName = ((FileArgument)parser.LookupArgument("output")).Value != null ?
-                                        ((FileArgument)parser.LookupArgument("output")).Value.FullName :
-                                        inputFileName;
-
-            Program p = new Program(connectionString, inputFileName, outputFileName);
+            var connectionstring = ((ValueArgument<SqlConnectionStringBuilder>)parser.LookupArgument("connectionstring")).Value.ConnectionString;
+            var inputFileName = ((FileArgument)parser.LookupArgument("input")).Value.FullName;
+            var outputFileName = ((FileArgument)parser.LookupArgument("output")).Value != null ? ((FileArgument)parser.LookupArgument("output")).Value.FullName : inputFileName;
+            var p = new Program(connectionstring, inputFileName, outputFileName);
 
             p.CreateDocumentation();
             p.Dispose();
         }
-        private static CommandLineParser.CommandLineParser CreateParser()
-        {
-            CommandLineParser.CommandLineParser parser = new CommandLineParser.CommandLineParser();
-            ValueArgument<SqlConnectionStringBuilder> connectionStringArgument = new ValueArgument<SqlConnectionStringBuilder>('c', "connectionString", "ConnectionString of the documented database")
-            {
+
+        private static CommandLineParser.CommandLineParser CreateParser() {
+            var parser = new CommandLineParser.CommandLineParser();
+            var connectionstringArgument = new ValueArgument<SqlConnectionStringBuilder>('c', "connectionstring", "Connectionstring of the documented database") {
                 Optional = false,
-                ConvertValueHandler = (stringValue) =>
-                {
-                    SqlConnectionStringBuilder connectionStringBuilder;
-                    try
-                    {
-                        connectionStringBuilder = new SqlConnectionStringBuilder(stringValue);
+                ConvertValueHandler = (stringValue) => {
+                    SqlConnectionStringBuilder connectionstringBuilder;
+
+                    try {
+                        connectionstringBuilder = new SqlConnectionStringBuilder(stringValue);
                     }
-                    catch
-                    {
-                        throw new InvalidConversionException("invalid connection string", "connectionString");
-                    }
-                    if (String.IsNullOrEmpty(connectionStringBuilder.InitialCatalog))
-                    {
-                        throw new InvalidConversionException("no InitialCatalog was specified", "connectionString");
+                    catch {
+                        throw new InvalidConversionException("invalid connection string", "connectionstring");
                     }
 
-                    return connectionStringBuilder;
+                    if (string.IsNullOrEmpty(connectionstringBuilder.InitialCatalog)) {
+                        throw new InvalidConversionException("no InitialCatalog was specified", "connectionstring");
+                    }
+
+                    return connectionstringBuilder;
                 }
             };
-            FileArgument inputFileArgument = new FileArgument('i', "input", "original edmx file") { FileMustExist = true, Optional = false };
-            FileArgument outputFileArgument = new FileArgument('o', "output", "output edmx file - Default : original edmx file") { FileMustExist = false, Optional = true };
+            var inputFileArgument = new FileArgument('i', "input", "original edmx file") { FileMustExist = true, Optional = false };
+            var outputFileArgument = new FileArgument('o', "output", "output edmx file - Default : original edmx file") { FileMustExist = false, Optional = true };
 
-            parser.Arguments.Add(connectionStringArgument);
+            parser.Arguments.Add(connectionstringArgument);
             parser.Arguments.Add(inputFileArgument);
             parser.Arguments.Add(outputFileArgument);
-
             parser.IgnoreCase = true;
+
             return parser;
         }
 
-
-        public String ConnectionString { get; set; }
-        public String InputFileName { get; set; }
-        public String OutputFileName { get; set; }
-
         private SqlConnection _connection;
 
-        public Program(String connectionString, String inputFileName, String outputFileName)
-        {
-            this.ConnectionString = connectionString;
-            this.InputFileName = inputFileName;
-            this.OutputFileName = outputFileName;
+        public string Connectionstring { get; set; }
+        public string InputFileName { get; set; }
+        public string OutputFileName { get; set; }
 
-            this._connection = new SqlConnection(connectionString);
-            this._connection.Open();
+        public Program(string connectionstring, string inputFileName, string outputFileName) {
+            Connectionstring = connectionstring;
+            InputFileName = inputFileName;
+            OutputFileName = outputFileName;
+
+            _connection = new SqlConnection(connectionstring);
+            _connection.Open();
         }
-        public void Dispose()
-        {
-            this._connection.Dispose();
+
+        public void Dispose() {
+            _connection.Dispose();
         }
 
-        private void CreateDocumentation()
-        {
-            XDocument doc = XDocument.Load(this.InputFileName);
+        private void CreateDocumentation() {
+            var doc = XDocument.Load(InputFileName);
 
-            if (doc.Root == null)
-            {
+            if (doc.Root == null) {
                 throw new Exception(string.Format("Loaded XDocument Root is null. File: {0}", InputFileName));
             }
+
             var entityTypeElements = doc.FindByLocalName("EntityType");
 
             int i = 0;
-            foreach (XElement entityTypeElement in entityTypeElements)
-            {
-                String tableName = entityTypeElement.Attribute("Name").Value;
+            foreach (var entityTypeElement in entityTypeElements) {
+                var tableName = entityTypeElement.Attribute("Name").Value;
                 var propertyElements = entityTypeElement.FindByLocalName("Property");
 
-                Console.Clear();
                 Console.WriteLine("Analyzing table {0} of {1}", i++, entityTypeElements.Count());
                 Console.WriteLine(" => TableName : {0}" +
                                   "\n => property count : {1}", tableName, propertyElements.Count());
+                Console.WriteLine(Environment.NewLine);
 
-                this.AddNodeDocumentation(entityTypeElement, GetTableDocumentation(tableName));
+                AddNodeDocumentation(entityTypeElement, GetTableDocumentation(tableName));
 
-                foreach (XElement propertyElement in propertyElements)
-                {
-                    String columnName = propertyElement.Attribute("Name").Value;
-                    this.AddNodeDocumentation(propertyElement, GetColumnDocumentation(tableName, columnName));
+                foreach (var propertyElement in propertyElements) {
+                    var columnName = propertyElement.Attribute("Name").Value;
+
+                    AddNodeDocumentation(propertyElement, GetColumnDocumentation(tableName, columnName));
                 }
             }
 
-            Console.WriteLine("Writing result to {0}", this.OutputFileName);
-            if (File.Exists(this.OutputFileName))
-                File.Delete(this.OutputFileName);
-            doc.Save(this.OutputFileName);
+            if (File.Exists(OutputFileName)) {
+                File.Delete(OutputFileName);
+            }
+
+            doc.Save(OutputFileName);
+
+            Console.WriteLine("Writing result to {0}", OutputFileName);
+            Console.WriteLine(Environment.NewLine);
+
+            #region add table and column summary
+            var contextFileName = InputFileName.Replace(".edmx", ".Context.tt");
+
+            if (File.Exists(contextFileName)) {
+                var text = File.ReadAllText(contextFileName);
+
+                text = text.Replace("<#=codeStringGenerator.DbSet(entitySet)#>", @"<#=""/// <summary>"" + Environment.NewLine + ""    "" + ""/// "" + ((entitySet.ElementType.Documentation != null) ? entitySet.ElementType.Documentation.Summary : """") + Environment.NewLine + ""    "" + ""/// </summary>"" + Environment.NewLine + ""    "" + codeStringGenerator.DbSet(entitySet)#>");
+
+                File.WriteAllText(contextFileName, text);
+
+                Console.WriteLine(@"Add table summary to {0}", contextFileName);
+                Console.WriteLine(Environment.NewLine);
+            }
+
+            var entityFileName = InputFileName.Replace(".edmx", ".tt");
+
+            if (File.Exists(entityFileName)) {
+                var text = File.ReadAllText(entityFileName);
+
+                text = text.Replace("<#=codeStringGenerator.EntityClassOpening(entity)#>", @"<#=""/// <summary>"" + Environment.NewLine + ""/// "" + ((entity.Documentation != null) ? entity.Documentation.Summary : """") + Environment.NewLine + ""/// </summary>"" + Environment.NewLine + codeStringGenerator.EntityClassOpening(entity)#>");
+                text = text.Replace("<#=codeStringGenerator.Property(edmProperty)#>", @"<#=""/// <summary>"" + Environment.NewLine + ""    "" + ""/// "" + ((edmProperty.Documentation != null) ? edmProperty.Documentation.Summary : """") + Environment.NewLine + ""    "" + ""/// </summary>"" + Environment.NewLine + ""    "" + codeStringGenerator.Property(edmProperty)#>");
+                text = text.Replace("<#=codeStringGenerator.Property(complexProperty)#>", @"<#=""/// <summary>"" + Environment.NewLine + ""    "" + ""/// "" + ((complexProperty.Documentation != null) ? complexProperty.Documentation.Summary : """") + Environment.NewLine + ""    "" + ""/// </summary>"" + Environment.NewLine + ""    "" + codeStringGenerator.Property(complexProperty)#>");
+
+                File.WriteAllText(entityFileName, text);
+
+                Console.WriteLine(@"Add column summary to {0}", entityFileName);
+                Console.WriteLine(Environment.NewLine);
+            }
+
+            if (File.Exists(contextFileName) && File.Exists(entityFileName)) {
+                Console.WriteLine(@"Please run ""Custom Tool"" to generate summary");
+                Console.WriteLine(@"Context => ""{0}""", Path.GetFileName(contextFileName));
+                Console.WriteLine(@"Entitys => ""{0}""", Path.GetFileName(entityFileName));
+                Console.WriteLine(Environment.NewLine);
+            }
+
+            Console.WriteLine(@"Operation is completed");
+            #endregion
         }
-        private void AddNodeDocumentation(XElement element, String documentation)
-        {
-            // remove stale documentation
+
+        private void AddNodeDocumentation(XElement element, string documentation) {
             element.FindByLocalName("Documentation").Remove();
 
-            if (String.IsNullOrEmpty(documentation))
+            if (string.IsNullOrEmpty(documentation)) {
                 return;
+            }
+
             var xmlns = element.GetDefaultNamespace();
 
             element.AddFirst(new XElement(xmlns + "Documentation", new XElement(xmlns + "Summary", documentation)));
         }
-        private String GetTableDocumentation(String tableName)
-        {
-            using (SqlCommand command = new SqlCommand(@" SELECT [value] 
-                                                          FROM fn_listextendedproperty (
-                                                                'MS_Description', 
-                                                                'schema', 'dbo', 
-                                                                'table',  @TableName, 
-                                                                null, null)", this._connection))
-            {
 
+        private string GetTableDocumentation(string tableName) {
+            using (var command = new SqlCommand(@"
+                SELECT [value] 
+                FROM fn_listextendedproperty (
+                    'MS_Description', 
+                    'schema', 'dbo', 
+                    'table',  @TableName, 
+                    null, null)", _connection)) {
                 command.Parameters.AddWithValue("TableName", tableName);
 
-                return command.ExecuteScalar() as String;
+                return command.ExecuteScalar() as string;
             }
         }
-        private String GetColumnDocumentation(String tableName, String columnName)
-        {
-            using (SqlCommand command = new SqlCommand(@"SELECT [value] 
-                                                         FROM fn_listextendedproperty (
-                                                                'MS_Description', 
-                                                                'schema', 'dbo', 
-                                                                'table', @TableName, 
-                                                                'column', @columnName)", this._connection))
-            {
 
+        private string GetColumnDocumentation(string tableName, string columnName) {
+            using (var command = new SqlCommand(@"
+                SELECT [value] 
+                FROM fn_listextendedproperty (
+                    'MS_Description', 
+                    'schema', 'dbo', 
+                    'table', @TableName, 
+                    'column', @columnName)", _connection)) {
                 command.Parameters.AddWithValue("TableName", tableName);
                 command.Parameters.AddWithValue("ColumnName", columnName);
 
-                return command.ExecuteScalar() as String;
+                return command.ExecuteScalar() as string;
             }
         }
     }
--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 About
 =====
 
-Project site: https://github.com/timabell/ef-document-generator
+Reads MSSQL table/column summary and then adds/updates the documentation in the matching `.emdx` and `.tt` files.
 
-Reads a db schema and then adds/updates the documentation in the matching `.emdx` file.
-
-This is a fork of http://eftsqldocgenerator.codeplex.com/ fixed-up for EF5
+This is a fork of https://github.com/timabell/ef-document-generator fixed-up for EF6
 
 Usage
 =====
@@ -20,28 +18,3 @@ Arguments
 * -c, --connectionString... ConnectionString of the documented database
 * -i, --input... original edmx file
 * -o, --output [optional] ... output edmx file - Default : original edmx file
-
-What now?
----------
-
-You may want to get the comments to be added to your generated model classes.
-You can do that by modifying your model.tt file as follows: https://gist.github.com/timabell/74bb6c6bbb7a4de843dc
-
-With that in place you will have consistent model documentation across your
-schema, edmx and c# model, with the tooling to keep them in sync.
-
-Licence
-=======
-
-Apache 2.0 as per upstream: http://eftsqldocgenerator.codeplex.com/license
-
-
-Download
-========
-
-Get the binary from https://github.com/timabell/ef-document-generator/releases/latest
-
-Links
-=====
-
-* Why? Because: http://stackoverflow.com/questions/2747788/how-can-i-make-the-entity-data-model-designer-use-my-database-column-descriptions
